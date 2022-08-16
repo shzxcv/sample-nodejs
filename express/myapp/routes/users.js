29ade91f4a7c836ae771672797c348d9cb7f79cc
shzxcv <sh.zxcv00@gmail.com>
2022-08-17 02:54:50 +0900

    add

---
+++ express/myapp/routes/users.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.put('/', function(req, res) {
+  res.send('Got a PUT request at /user')
+})
+
+module.exports = router;
